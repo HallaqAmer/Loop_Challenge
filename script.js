5f2a34aa8e3100143bc21254660ff9ad787d104a
Amer Hallaq <amerhalaq91@gmail.com>
2022-10-27 19:45:29 +0300

    Initial Commit

---
+++ script.js
@@ -0,0 +1,23 @@
+for(var i=1;i<=20;i++) {  //Challenge#1
+    if(i%2!=0) {
+        console.log(i)
+    }
+}
+for(var j=100;j>=0;j--) {  //Challenge#2
+    if(j%3==0) {
+        console.log(j)
+    }
+}
+for(var i=4;i>-4;i-=1.5) {  //Challenge#3
+    console.log(i)
+}
+var sum=0
+for(i=1;i<=100;i++) {     //Challenge#4
+    sum+=i
+}
+console.log(sum)
+var product=1
+for(i=1;i<=12;i++) {     //Challenge#5
+    product*=i
+}
+console.log(product)
